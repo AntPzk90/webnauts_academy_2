dc91dc1283af0aa3792e36b6b72c5e6d10adf3e5
AntPzk90 <44503472+AntPzk90@users.noreply.github.com>
2021-06-29 21:36:42 +0300

    add slider

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,32 @@
-let popUp = document.querySelector(".pop-up");
-let popUpBtn = document.querySelector(".btn-pop-up");
-let popUpClose = popUp.querySelector(".pop-up-close");
-let footerBtn = document.querySelector(".btn-dark");
+// let popUp = document.querySelector(".pop-up");
+// let popUpBtn = document.querySelector(".btn-pop-up");
+// let popUpClose = popUp.querySelector(".pop-up-close");
+// let footerBtn = document.querySelector(".btn-dark");
 
-function showPopUpOnClick() {
-  popUp.classList.add("pop-up-show");
-}
+// function showPopUpOnClick() {
+//   popUp.classList.add("pop-up-show");
+// }
 
-function closePopUpOnClick() {
-  popUp.classList.remove("pop-up-show");
-}
+// function closePopUpOnClick() {
+//   popUp.classList.remove("pop-up-show");
+// }
 
-function closePopUpOnClick() {
-    popUp.classList.remove("pop-up-show");
-}
+// popUpBtn.addEventListener("click", showPopUpOnClick);
+// footerBtn.addEventListener("click", showPopUpOnClick);
+// popUpClose.addEventListener("click", closePopUpOnClick);
 
-popUpBtn.addEventListener("click", showPopUpOnClick);
-footerBtn.addEventListener("click", showPopUpOnClick);
-popUpClose.addEventListener("click", closePopUpOnClick);
+let swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1.2,
+  centeredSlides: true,
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
